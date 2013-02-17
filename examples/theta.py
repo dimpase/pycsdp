9ae12a11256ff1abb1522d4bcd443af4aa6b6eed
Dima Pasechnik <dima@ntu.edu.sg>
2013-02-18 01:25:46 +0800

    added not yet working theta for Cayley graphs

--- examples/theta.py
+++ examples/theta.py
@@ -27,3 +27,51 @@ def _theta_graph(n, e): # this should work in the same way as theta in csdp
    sol = _csdp.Solver(C,A,b)
    return sol.solution()
 
+def _theta_cayley_graph(invs, perms, rest_invs, rest_perms):
+# Cayley graph of an n-element group is given by its generators, 
+# involutory(invs)+noninvolutory(perms); each generator is given as a list
+# of numbers from 1 to n 
+# e.g.  _theta_cayley_graph([], [[2,3,4,5,1]], [], [[3,4,5,1,2]]) should give sqrt(5.0)
+#
+   import numpy as np
+   from pycsdp import _csdp
+   from sys import getrefcount # why do we need this?
+
+   if invs == []:
+      n = len(perms[0])
+   else:
+      n = len(invs[0])
+      
+   b = [float(n)]*len(invs)+[float(n)*2.0]*len(perms)+ \
+       [0.0]*(len(rest_invs)+len(rest_perms)) # RHS
+   C = _csdp.Objective([-np.identity(n)]) # objective
+
+   cA = []
+   for p in invs: 
+      cA.append((
+          (n,np.array(range(1,n+1)), 
+             np.array([int(j) for j in p]), 
+             np.array([1.0]*n)), None))
+
+   for p in perms: 
+      cA.append((
+          (n,np.array(range(1,n+1)+[int(j) for j in p]), 
+             np.array([int(j) for j in p]+range(1,n+1)), 
+             np.array([1.0]*(2*n))), None))
+
+   for p in rest_invs: 
+      cA.append((
+          (n,np.array(range(1,n+1)), 
+             np.array([int(j) for j in p]), 
+             np.array([1.0]*n)), None))
+
+   for p in rest_perms: 
+      cA.append((
+          (n,np.array(range(1,n+1)+[int(j) for j in p]), 
+             np.array([int(j) for j in p]+range(1,n+1)), 
+             np.array([1.0]*(2*n))), None))
+
+   A = _csdp.Constraints(cA)
+   sol = _csdp.Solver(C,A,b)
+ 
+   return sol.solution()

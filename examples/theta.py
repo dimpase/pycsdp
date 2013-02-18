807cc599f47eeda124ff7ce2866056954b50019f
Dima Pasechnik <dima@ntu.edu.sg>
2013-02-18 17:28:14 +0800

    fixed theta computations

--- examples/theta.py
+++ examples/theta.py
@@ -20,56 +20,58 @@ def _theta_graph(n, e): # this should work in the same way as theta in csdp
          np.array([1.0]*n)) # constraint Tr(X)==1
    cA = [(A1,None)]
    for i,j in e:
-      cA.append(((n,np.array([i+1,j+1]), np.array([j+1,i+1]), np.array([1.0,1.0])),None))
+      if i<j:
+         cA.append(((n,np.array([i+1]), np.array([j+1]), np.array([1.0])),None))
+      else:
+         cA.append(((n,np.array([j+1]), np.array([i+1]), np.array([1.0])),None))
 
    A = _csdp.Constraints(cA)
    b = [1.0]+[0.0]*len(e)
    sol = _csdp.Solver(C,A,b)
    return sol.solution()
 
-def _theta_cayley_graph(invs, perms, rest_invs, rest_perms):
+def _theta_cayley_graph(invs, perms):
 # Cayley graph of an n-element group is given by its generators, 
 # involutory(invs)+noninvolutory(perms); each generator is given as a list
 # of numbers from 1 to n 
-# e.g.  _theta_cayley_graph([], [[2,3,4,5,1]], [], [[3,4,5,1,2]]) should give sqrt(5.0)
+# e.g. 1-obj(_theta_cayley_graph([], [[2,3,4,5,1]])) should give sqrt(5.0),
+# the value of theta for the 5-gon
 #
    import numpy as np
    from pycsdp import _csdp
    from sys import getrefcount # why do we need this?
-
    if invs == []:
       n = len(perms[0])
    else:
       n = len(invs[0])
-      
-   b = [float(n)]*len(invs)+[float(n)*2.0]*len(perms)+ \
-       [0.0]*(len(rest_invs)+len(rest_perms)) # RHS
-   C = _csdp.Objective([-np.identity(n)]) # objective
+   
+   b = [-1.0*float(n)]*len(invs)+[-float(n)*2.0]*len(perms)
+   c0 = -np.identity(n)/float(n)
+   C = _csdp.Objective([c0]) # objective
 
    cA = []
    for p in invs: 
+      p1=[]
+      p2=[]
+      for i in range(n):
+         if i+1<p[i]:
+            p1.append(i+1)   
+            p2.append(p[i])
       cA.append((
-          (n,np.array(range(1,n+1)), 
-             np.array([int(j) for j in p]), 
-             np.array([1.0]*n)), None))
+          (n,np.array(p1), np.array(p2), np.array([1.0]*(n/2))), None))
 
    for p in perms: 
+      p1=[]
+      p2=[]
+      for i in range(n):
+         if i+1<p[i]:
+            p1.append(i+1)   
+            p2.append(p[i])
+         else:
+            p2.append(i+1)   
+            p1.append(p[i])
       cA.append((
-          (n,np.array(range(1,n+1)+[int(j) for j in p]), 
-             np.array([int(j) for j in p]+range(1,n+1)), 
-             np.array([1.0]*(2*n))), None))
-
-   for p in rest_invs: 
-      cA.append((
-          (n,np.array(range(1,n+1)), 
-             np.array([int(j) for j in p]), 
-             np.array([1.0]*n)), None))
-
-   for p in rest_perms: 
-      cA.append((
-          (n,np.array(range(1,n+1)+[int(j) for j in p]), 
-             np.array([int(j) for j in p]+range(1,n+1)), 
-             np.array([1.0]*(2*n))), None))
+          (n,np.array(p1), np.array(p2), np.array([1.0]*n)), None))
 
    A = _csdp.Constraints(cA)
    sol = _csdp.Solver(C,A,b)
